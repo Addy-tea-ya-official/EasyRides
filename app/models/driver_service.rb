187e769ee621f1787734ae9f95179ca21c4ebc8d
Aditya Yadav <aditya.yadav19@pccoepune.org>
2022-09-09 19:02:55 +0530

    Update foreign key constraints of driver_service and service_ticket to optional with user, they have different foreign key column names

--- app/models/driver_service.rb
+++ app/models/driver_service.rb
@@ -1,5 +1,5 @@
 class DriverService < ApplicationRecord
   has_many :service_tickets
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :vehicle
 end
--- app/models/service_ticket.rb
+++ app/models/service_ticket.rb
@@ -1,4 +1,4 @@
 class ServiceTicket < ApplicationRecord
-  belongs_to :user
+  belongs_to :user,optional: true
   belongs_to :driver_service
 end
